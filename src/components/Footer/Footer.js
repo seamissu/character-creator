7a5da1f2982ef46958d209f03a89d195037ec2e2
Jingning <seamissu@gmail.com>
2023-09-04 03:53:42 +1000

    Finished Exercises 1-4

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       All character assets taken from{' '}
       <a
+        className={styles.a}
         href="https://www.openpeeps.com/"
         target="_blank"
         rel="noopener noreferrer"
